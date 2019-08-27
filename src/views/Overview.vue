40eb6a0714eba6dea81a9ab1507bec143724d708
tony <tony_wangzt@163.com>
2019-08-27 17:45:19 +0800

    add

--- src/views/Overview.vue
+++ src/views/Overview.vue
@@ -1,15 +1,27 @@
 <template>
-  <el-card class="box-card">
-    
-    <div v-for="o in 4" :key="o" class="text item">{{'列表内容 ' + o }}</div>
+  <div class="card">
+      <el-card class="box-card">
+    <div> <img src="https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1566906522988&di=da2f84094e3916b0329c64a6b8fd6872&imgtype=0&src=http%3A%2F%2Fimage.bubuko.com%2Finfo%2F201801%2F20180114231439532897.png" alt="" height="40px" width="160px"></div>
+    <p>集群数量: {{ elasticsearchnum }}</p>
   </el-card>
+  <el-card class="box-card">
+    <div> <img src="https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1566906522988&di=da2f84094e3916b0329c64a6b8fd6872&imgtype=0&src=http%3A%2F%2Fimage.bubuko.com%2Finfo%2F201801%2F20180114231439532897.png" alt="" height="40px" width="160px"></div>
+    <p>集群数量: {{ elasticsearchnum }}</p>
+  </el-card>
+  </div>
   
 
 
 </template>
 
 <script>
-export default {};
+export default {
+    data(){
+        return {
+            elasticsearchnum: 100
+        };
+    }
+};
 </script>
 
 <style scoped>
@@ -21,10 +33,16 @@ export default {};
   margin-bottom: 18px;
 }
 
+.card {
+    display: flex;
+    justify-content: flex-start;
+}
+
 .box-card {
   margin: 20px;
   width: 200px;
   display: flex;
+  text-align: center;
   justify-content: center;
 }
 </style>
