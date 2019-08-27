d6619464f4e2ed197d747a3c123187c87cb67b32
tony <tony_wangzt@163.com>
2019-08-27 17:19:27 +0800

    first commit

---
+++ src/router.js
@@ -0,0 +1,41 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Home from '@/components/base/Home.vue' 
+import Centent from '@/views/Centent.vue'
+import Overview from '@/views/Overview.vue'
+
+
+Vue.use(Router)
+
+export default new Router({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes: [
+    
+    {
+      path: '/about',
+      name: 'about',
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import(/* webpackChunkName: "about" */ '@/views/Centent.vue')
+    },{
+      path: '/',
+      name: '',
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: Home,
+      children: [
+        {
+          path: 'centent',
+          component: Centent
+        },
+        {
+          path: 'overview',
+          component: Overview
+        }
+      ]
+    }
+  ]
+})
---
+++ src/views/Overview.vue
@@ -0,0 +1,30 @@
+<template>
+  <el-card class="box-card">
+    
+    <div v-for="o in 4" :key="o" class="text item">{{'列表内容 ' + o }}</div>
+  </el-card>
+  
+
+
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.text {
+  font-size: 14px;
+}
+
+.item {
+  margin-bottom: 18px;
+}
+
+.box-card {
+  margin: 20px;
+  width: 200px;
+  display: flex;
+  justify-content: center;
+}
+</style>
