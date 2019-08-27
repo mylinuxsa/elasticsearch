050434f62cc2b44e4b610d9a50ba1b0a718ef160
tony <tony_wangzt@163.com>
2019-08-27 17:49:11 +0800

    m

--- src/views/Overview.vue
+++ src/views/Overview.vue
@@ -1,6 +1,18 @@
 <template>
   <div class="card">
       <el-card class="box-card">
+    <div> <img src="https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1566906522988&di=da2f84094e3916b0329c64a6b8fd6872&imgtype=0&src=http%3A%2F%2Fimage.bubuko.com%2Finfo%2F201801%2F20180114231439532897.png" alt="" height="80px" width="320px"></div>
+    <p>集群数量: {{ elasticsearchnum }}</p>
+  </el-card>
+  <el-card class="box-card">
+    <div> <img src="https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1566906522988&di=da2f84094e3916b0329c64a6b8fd6872&imgtype=0&src=http%3A%2F%2Fimage.bubuko.com%2Finfo%2F201801%2F20180114231439532897.png" alt="" height="40px" width="160px"></div>
+    <p>集群数量: {{ elasticsearchnum }}</p>
+  </el-card>
+  <el-card class="box-card">
+    <div> <img src="https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1566906522988&di=da2f84094e3916b0329c64a6b8fd6872&imgtype=0&src=http%3A%2F%2Fimage.bubuko.com%2Finfo%2F201801%2F20180114231439532897.png" alt="" height="40px" width="160px"></div>
+    <p>集群数量: {{ elasticsearchnum }}</p>
+  </el-card>
+  <el-card class="box-card">
     <div> <img src="https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1566906522988&di=da2f84094e3916b0329c64a6b8fd6872&imgtype=0&src=http%3A%2F%2Fimage.bubuko.com%2Finfo%2F201801%2F20180114231439532897.png" alt="" height="40px" width="160px"></div>
     <p>集群数量: {{ elasticsearchnum }}</p>
   </el-card>
@@ -33,16 +45,13 @@ export default {
   margin-bottom: 18px;
 }
 
-.card {
-    display: flex;
-    justify-content: flex-start;
-}
+
 
 .box-card {
   margin: 20px;
-  width: 200px;
-  display: flex;
+  width: 480px;
+  height: 240px;
   text-align: center;
-  justify-content: center;
+  float: left;
 }
 </style>
